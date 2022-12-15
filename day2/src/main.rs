399ac071cba5896f7d86cd39d2530c2aa6ad5c37
zhesnaile <carlosojar@gmail.com>
2022-12-16 00:42:57 +0100

    [IMP] useless change, aligned match fields.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -22,14 +22,12 @@ fn find_move(a: &str) -> RPSMove {
         _ => panic!("Invalid value read")
     }
 }
-/**
- * 
- */
+
 fn play_round(rival_move :RPSMove, player_move: RPSMove) -> RPSMatchResult {
     let round = (rival_move, player_move);
     
     match round {
-        (RPSMove::Rock, RPSMove::Paper)| (RPSMove::Paper, RPSMove::Scissors) | (RPSMove::Scissors, RPSMove::Rock) => RPSMatchResult::Win,
+        (RPSMove::Rock, RPSMove::Paper) | (RPSMove::Paper, RPSMove::Scissors) | (RPSMove::Scissors, RPSMove::Rock) => RPSMatchResult::Win,
         (RPSMove::Paper, RPSMove::Rock) | (RPSMove::Scissors, RPSMove::Paper) | (RPSMove::Rock, RPSMove::Scissors) => RPSMatchResult::Loss,
         (_, _) => RPSMatchResult::Draw,
     }
