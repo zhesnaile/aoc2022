33afc19eb44c79b0be5cbc40cf675f7381b66868
zhesnaile <carlosojar@gmail.com>
2022-12-16 00:51:39 +0100

    [IMP] Cargo FMT

--- day2/src/bin/rps_part_two.rs
+++ day2/src/bin/rps_part_two.rs
@@ -1,10 +1,13 @@
-use std::{io::{self, BufReader, BufRead}, fs::File};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 #[derive(Copy, Clone)]
 enum RPSMatchResult {
     Loss = 0,
     Draw = 3,
-    Win = 6
+    Win = 6,
 }
 
 #[derive(Copy, Clone)]
@@ -16,22 +19,25 @@ enum RPSMove {
 
 fn find_move(a: &str) -> RPSMove {
     match a {
-        "A"|"X" => RPSMove::Rock,
-        "B"|"Y" => RPSMove::Paper,
-        "C"|"Z" => RPSMove::Scissors,
-        _ => panic!("Invalid value read")
+        "A" | "X" => RPSMove::Rock,
+        "B" | "Y" => RPSMove::Paper,
+        "C" | "Z" => RPSMove::Scissors,
+        _ => panic!("Invalid value read"),
     }
 }
 
-fn play_round(rival_move :RPSMove, player_move: RPSMove) -> RPSMatchResult {
+fn play_round(rival_move: RPSMove, player_move: RPSMove) -> RPSMatchResult {
     let round = (rival_move, player_move);
-    
+
     match round {
-        (RPSMove::Rock, RPSMove::Paper) | (RPSMove::Paper, RPSMove::Scissors) | (RPSMove::Scissors, RPSMove::Rock) => RPSMatchResult::Win,
-        (RPSMove::Paper, RPSMove::Rock) | (RPSMove::Scissors, RPSMove::Paper) | (RPSMove::Rock, RPSMove::Scissors) => RPSMatchResult::Loss,
+        (RPSMove::Rock, RPSMove::Paper)
+        | (RPSMove::Paper, RPSMove::Scissors)
+        | (RPSMove::Scissors, RPSMove::Rock) => RPSMatchResult::Win,
+        (RPSMove::Paper, RPSMove::Rock)
+        | (RPSMove::Scissors, RPSMove::Paper)
+        | (RPSMove::Rock, RPSMove::Scissors) => RPSMatchResult::Loss,
         (_, _) => RPSMatchResult::Draw,
     }
-
 }
 
 fn main() -> io::Result<()> {
@@ -43,11 +49,11 @@ fn main() -> io::Result<()> {
     for line in reader.lines() {
         let line = line?;
         let rps_match: Vec<&str> = line.split(' ').collect();
-        
+
         let rival_move = find_move(rps_match[0]);
         let player_move = find_move(rps_match[1]);
-        
-        let round_points= play_round(rival_move, player_move) as isize + player_move as isize; 
+
+        let round_points = play_round(rival_move, player_move) as isize + player_move as isize;
 
         player_points += round_points;
     }
